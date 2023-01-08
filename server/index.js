6a2dba86d9bf6ce05d20ebdf432d6a191ac36f9d
Sandor Desi <d.alex.galaxy@gmail.com>
2023-01-08 18:23:14 +0000

    Base for the app

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const schema = require("./schema/schema");
+const { graphqlHTTP } = require("express-graphql");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port${port} `));
